0e4218d5774d752c7edabf641bd2442509ff5fee
Thyago2321 <thyaguin22345@gmail.com>
2026-05-10 17:15:05 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Glow Up em 30 Dias',
+  description: 'Guia feminino de autoestima'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  )
+}
